d0fda8437f22b57c5970825618755bca3ef0aa30
savra0923 <savra0923@gmail.com>
2024-03-19 13:43:05 +0200

    got pong, now need to loop

--- instance1.py
+++ instance1.py
@@ -5,14 +5,15 @@ import asyncio
 import sys
 import time
 
-app = FastAPI()
+app1 = FastAPI()
+app2 = FastAPI()
 
 pong_time_ms = 1000  # Milliseconds between pings
 instance1_url = "http://localhost:8000"
 instance2_url = "http://localhost:8001"
+ping_interval = pong_time_ms / 1000
 
-
-@app.get("/")
+@app1.get("/")
 async def ping_instance():
     try:
         response = requests.get(instance2_url + "/ping")
@@ -21,22 +22,22 @@ async def ping_instance():
         raise HTTPException(status_code=500, detail=f"Error pinging instance2: {e}")
 
 
-@app.get("/ping")
+@app1.get("/ping")
 async def ping():
     print(f"recieved PING")
     try:
         response = requests.get(instance2_url + "/pong")
         return response.text
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error pinging instance2: {e}")
+        raise HTTPException(status_code=501, detail=f"Error pinging instance2: {e}")
 
 
-@app.get("/pong")
+@app2.get("/pong")
 async def pong():
-    print(f"HIIII PONG")
+    print(f"recieved PONG")
     try:
         await asyncio.sleep(pong_time_ms / 1000)
         response = requests.get(instance1_url + "/ping")
         return response.text
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error pinging instance1: {e}")
+        raise HTTPException(status_code=502, detail=f"Error pinging instance1: {e}")
